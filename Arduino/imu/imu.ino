9b00d61e3bab75c4be92856d38d0b07f9d47fd5a
xChlo11 <cajs.strick10@gmail.com>
2018-07-07 14:39:21 -0400

    SV 1st commit

--- Arduino/imu/imu.ino
+++ Arduino/imu/imu.ino
@@ -11,7 +11,9 @@
 
 const double pi = 3.14159265358979;
 const double gyro_deg2rad = 500.0 * pi / 5898240.0;
-
+float rollGyro;
+float rollAcc;
+float alpha = .97;
 int i,j;
 
 int32_t a[3];
@@ -33,14 +35,44 @@ void setup()
   //Check that the device ID stored in the WHO_AM_I register matches the known ID of 0x71 for the MPU9255
   //This is to ensure that the device is properly connected
   device_id = read(MPU9255_ADDRESS, WHO_AM_I, 1);
-  //Serial.println(device_id == 0x71 ? "Communication with MPU9255 successful" : "MPU9255 not found");
+ // Serial.println(device_id == 0x71 ? "Communication with MPU9255 successful" : "MPU9255 not found");
   //while(!(device_id == 0x71));
 
+  Wire.beginTransmission(MPU9255_ADDRESS);
+  Wire.write(0x3B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(MPU9255_ADDRESS);
+  Wire.write(0x3C);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  
   dt = micros();
 }
 
 void loop() 
 {
+for (int i=0; i<=2; i++)
+{
+  a[i] = read(MPU9255_ADDRESS, acc_address[i], 2);
+  g[i] = read(MPU9255_ADDRESS, gyro_address[i], 2);
+}
+/*Serial.print(a[0]); Serial.print(" ");
+Serial.print(a[1]); Serial.print(" ");
+Serial.print(a[2]); Serial.print(" ");
+
+Serial.print(g[0]); Serial.print(" ");
+Serial.print(g[1]); Serial.print(" ");
+Serial.println(g[2]);*/
+
+rollRate= g[1]*gyro_deg2rad;
+rollGyro = rollGyro + (rollRate * ((micros() - dt))/1000000.0);
+rollAcc=atan2(a[0], a[2]);
+dt= micros();
+
+rollAngle = alpha * (rollAngle + rollRate * (micros() - dt)/1000000.0) + (1 - alpha) * rollAcc;
+Serial.print(rollAngle); Serial.print(" "); Serial.println(rollRate);// Serial.print(" "); Serial.println(rollAcc);
 
 }
 
